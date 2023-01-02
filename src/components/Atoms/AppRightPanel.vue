0f5a07e42d35fee8a4a76f646c8519c0eada903a
rob <dev@mexion.com>
2023-01-02 18:07:44 +0100

    delete temp img

--- src/components/Atoms/AppRightPanel.vue
+++ src/components/Atoms/AppRightPanel.vue
@@ -12,22 +12,7 @@
         <div class="online-members flex flex-column mt-3" :class="'ml-3'">
             <h6 class="header">ONLINE MEMBERS</h6>
             <div class="flex flex-row flex-wrap">
-                <img class="m-1" src="../../../public/demo/images/avatar/avatar-1.png" alt="avatar-1" />
-                <img class="m-1" src="../../../public/demo/images/avatar/avatar-2.png" alt="avatar-2" />
-                <img class="m-1" src="../../../public/demo/images/avatar/avatar-3.png" alt="avatar-3" />
-                <img class="m-1" src="../../../public/demo/images/avatar/avatar-4.png" alt="avatar-4" />
-                <img class="m-1" src="../../../public/demo/images/avatar/avatar-5.png" alt="avatar-5" />
-                <img class="m-1" src="../../../public/demo/images/avatar/avatar-6.png" alt="avatar-6" />
-                <img class="m-1" src="../../../public/demo/images/avatar/avatar-7.png" alt="avatar-7" />
-                <img class="m-1" src="../../../public/demo/images/avatar/avatar-8.png" alt="avatar-8" />
-                <img class="m-1" src="../../../public/demo/images/avatar/avatar-9.png" alt="avatar-9" />
-                <img class="m-1" src="../../../public/demo/images/avatar/avatar-10.png" alt="avatar-10" />
-                <img class="m-1" src="../../../public/demo/images/avatar/avatar-11.png" alt="avatar-11" />
-                <img class="m-1" src="../../../public/demo/images/avatar/avatar-12.png" alt="avatar-12" />
-                <img class="m-1" src="../../../public/demo/images/avatar/avatar-13.png" alt="avatar-13" />
-                <img class="m-1" src="../../../public/demo/images/avatar/avatar-14.png" alt="avatar-14" />
-                <img class="m-1" src="../../../public/demo/images/avatar/avatar-15.png" alt="avatar-15" />
-                <img class="m-1" src="../../../public/demo/images/avatar/avatar-16.png" alt="avatar-16" />
+                <img class="m-1" src="" alt="avatar-1" />
             </div>
             <span class="mt-3"><b>+19</b> Costumers</span>
         </div>
@@ -39,7 +24,7 @@
                     <span class="font-bold mb-1">New Sale</span>
                     <span class="mb-2">Richard Jones has purchased a blue t-shirt for $79.</span>
                     <span class="flex align-items-center">
-                        <img :class="'mr-2'" src="../../../public/demo/images/avatar/activity-1.png" alt="" />
+                        <img :class="'mr-2'" src="" alt="" />
                         <small class="muted-text">Emmy Adams, 21.40</small>
                     </span>
                 </div>
@@ -50,7 +35,7 @@
                     <span class="font-bold mb-1">Withdrawal Initiated</span>
                     <span class="mb-2">Your request for withdrawal of $2500 has been initiated.</span>
                     <span class="flex align-items-center">
-                        <img :class="'mr-2'" src="../../../public/demo/images/avatar/activity-2.png" alt="avatar-2" />
+                        <img :class="'mr-2'" src="" alt="avatar-2" />
                         <small class="muted-text">Emily Walter, 21.40</small>
                     </span>
                 </div>
@@ -61,7 +46,7 @@
                     <span class="font-bold mb-1">Question Received</span>
                     <span class="mb-2">Jane Davis has posted a new question about your product.</span>
                     <span class="flex align-items-center">
-                        <img :class="'mr-2'" src="../../../public/demo/images/avatar/activity-3.png" alt="avatar-3" />
+                        <img :class="'mr-2'" src="" alt="avatar-3" />
                         <small class="muted-text">Jane Davis, 21.45</small>
                     </span>
                 </div>
