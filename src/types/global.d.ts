d3c5a0f920e691e46052cf99d5201473db9ed62f
rob <dev@mexion.com>
2023-01-03 17:15:21 +0100

    global type, not working

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -1,22 +1,13 @@
 // type Nullable<T> = T | undefined | null;
 
-class LeftMenuItem {
-    to?: string;
-    badgeStyleClass?: string | undefined
-    items?: LeftMenuItem[] | undefined;
-    class?: string;
-    style?: string;
-    target?: string;
-    url?: string;
-    disabled?: boolean | undefined
-    badgeStyle?: string | undefined
-    label?: string;
-    badge?: string;
-    icon?: string;
-    separator?: boolean;
+import {PropType} from "vue";
 
-    [key: string]: any;
+class BrandingData {
+    logo: {
+        light: string;
+        dark: string;
+        title: string;
+        height: string;
+    };
 }
-
-
 
